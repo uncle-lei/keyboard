07e5f3ca82ce16530856dcf4bb5557bb52291410
uncle-lei <307870736@qq.com>
2020-08-27 10:06:54 +0800

    keyboard备份

---
+++ keyboard.cpp
@@ -0,0 +1,347 @@
+﻿#pragma execution_character_set("utf-8")
+#include "keyboard.h"
+#include "ui_keyboard.h"
+#include "qdesktopwidget.h"
+
+//Keyboard *Keyboard::_instance = 0;
+Keyboard::Keyboard(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Keyboard)
+{
+    ui->setupUi(this);
+
+
+}
+
+Keyboard::~Keyboard()
+{
+    delete ui;
+}
+
+void Keyboard::Init(int btnFontSize , int labFontSize )
+{
+    this->btnFontSize = btnFontSize;
+    this->labFontSize = labFontSize;
+    this->ChangeFont();
+    this->InitProperty();
+    this->InitForm();
+
+}
+
+void Keyboard::mouseMoveEvent(QMouseEvent *e)
+{
+    if (mousePressed && (e->buttons() && Qt::LeftButton)) {
+        this->move(e->globalPos() - mousePoint);
+        e->accept();
+    }
+}
+
+void Keyboard::mousePressEvent(QMouseEvent *e)
+{
+    if (e->button() == Qt::LeftButton) {
+        mousePressed = true;
+        mousePoint = e->globalPos() - this->pos();
+        e->accept();
+    }
+}
+
+void Keyboard::mouseReleaseEvent(QMouseEvent *)
+{
+    mousePressed = false;
+}
+
+void Keyboard:: InitForm()
+{
+    this->mousePressed = false;
+
+    frmWidth = this->width();
+    frmHeight = this->height();
+    currentWidget = 0;
+    currentLineEdit = 0;
+    currentTextEdit = 0;
+    currentPlain = 0;
+    currentBrowser = 0;
+    currentEditType = "";
+
+    //如果需要更改输入法初始模式,改变currentType这个变量即可
+    //min--小写模式  max--大写模式  chinese--中文模式
+    currentType = "min";
+    changeType(currentType);
+
+    QList<QPushButton *> btn = this->findChildren<QPushButton *>();
+    foreach (QPushButton * b, btn) {
+        connect(b, SIGNAL(clicked()), this, SLOT(btn_clicked()));
+    }
+}
+
+void Keyboard::InitProperty()
+{
+    ui->btnOther1->setProperty("btnOther", true);
+    ui->btnOther2->setProperty("btnOther", true);
+    ui->btnOther3->setProperty("btnOther", true);
+    ui->btnOther4->setProperty("btnOther", true);
+    ui->btnOther5->setProperty("btnOther", true);
+    ui->btnOther6->setProperty("btnOther", true);
+    ui->btnOther7->setProperty("btnOther", true);
+    ui->btnOther8->setProperty("btnOther", true);
+    ui->btnOther9->setProperty("btnOther", true);
+    ui->btnOther10->setProperty("btnOther", true);
+    ui->btnOther11->setProperty("btnOther", true);
+    ui->btnOther12->setProperty("btnOther", true);
+    ui->btnOther13->setProperty("btnOther", true);
+    ui->btnOther14->setProperty("btnOther", true);
+    ui->btnOther15->setProperty("btnOther", true);
+    ui->btnOther16->setProperty("btnOther", true);
+    ui->btnOther17->setProperty("btnOther", true);
+    ui->btnOther18->setProperty("btnOther", true);
+    ui->btnOther19->setProperty("btnOther", true);
+    ui->btnOther20->setProperty("btnOther", true);
+    ui->btnOther21->setProperty("btnOther", true);
+
+    ui->btnDot->setProperty("btnOther", true);
+    ui->btnSpace->setProperty("btnOther", true);
+    ui->btnDelete->setProperty("btnOther", true);
+    ui->Enter->setProperty("btnOther",true);
+    ui->ans0->setProperty("btnOther",true);
+    ui->ans1->setProperty("btnOther",true);
+    ui->ans2->setProperty("btnOther",true);
+
+    ui->btn0->setProperty("btnNum", true);
+    ui->btn1->setProperty("btnNum", true);
+    ui->btn2->setProperty("btnNum", true);
+    ui->btn3->setProperty("btnNum", true);
+    ui->btn4->setProperty("btnNum", true);
+    ui->btn5->setProperty("btnNum", true);
+    ui->btn6->setProperty("btnNum", true);
+    ui->btn7->setProperty("btnNum", true);
+    ui->btn8->setProperty("btnNum", true);
+    ui->btn9->setProperty("btnNum", true);
+    ui->btn00->setProperty("btnNum", true);
+
+    ui->btna->setProperty("btnLetter", true);
+    ui->btnb->setProperty("btnLetter", true);
+    ui->btnc->setProperty("btnLetter", true);
+    ui->btnd->setProperty("btnLetter", true);
+    ui->btne->setProperty("btnLetter", true);
+    ui->btnf->setProperty("btnLetter", true);
+    ui->btng->setProperty("btnLetter", true);
+    ui->btnh->setProperty("btnLetter", true);
+    ui->btni->setProperty("btnLetter", true);
+    ui->btnj->setProperty("btnLetter", true);
+    ui->btnk->setProperty("btnLetter", true);
+    ui->btnl->setProperty("btnLetter", true);
+    ui->btnm->setProperty("btnLetter", true);
+    ui->btnn->setProperty("btnLetter", true);
+    ui->btno->setProperty("btnLetter", true);
+    ui->btnp->setProperty("btnLetter", true);
+    ui->btnq->setProperty("btnLetter", true);
+    ui->btnr->setProperty("btnLetter", true);
+    ui->btns->setProperty("btnLetter", true);
+    ui->btnt->setProperty("btnLetter", true);
+    ui->btnu->setProperty("btnLetter", true);
+    ui->btnv->setProperty("btnLetter", true);
+    ui->btnw->setProperty("btnLetter", true);
+    ui->btnx->setProperty("btnLetter", true);
+    ui->btny->setProperty("btnLetter", true);
+    ui->btnz->setProperty("btnLetter", true);
+}
+
+void Keyboard::ShowPanel()
+{
+    this->setVisible(true);
+
+}
+
+void Keyboard::bindWidget(QWidget *nowWidget)
+{
+    if (nowWidget->inherits("QLineEdit")) {
+        currentLineEdit = (QLineEdit *)nowWidget;
+        currentEditType = "QLineEdit";
+        ShowPanel();
+    } else if (nowWidget->inherits("QTextEdit")) {
+        currentTextEdit = (QTextEdit *)nowWidget;
+        currentEditType = "QTextEdit";
+        ShowPanel();
+    } else if (nowWidget->inherits("QPlainTextEdit")) {
+        currentPlain = (QPlainTextEdit *)nowWidget;
+        currentEditType = "QPlainTextEdit";
+        ShowPanel();
+    } else if (nowWidget->inherits("QTextBrowser")) {
+        currentBrowser = (QTextBrowser *)nowWidget;
+        currentEditType = "QTextBrowser";
+        ShowPanel();
+    } else if (nowWidget->inherits("QComboBox")) {
+        QComboBox *cbox = (QComboBox *)nowWidget;
+        //只有当下拉选择框处于编辑模式才可以输入
+        if (cbox->isEditable()) {
+            currentLineEdit = cbox->lineEdit() ;
+            currentEditType = "QLineEdit";
+            ShowPanel();
+        }
+    } else  if(nowWidget->inherits("QSpinBox") ||
+               nowWidget->inherits("QDoubleSpinBox") ||
+               nowWidget->inherits("QDateEdit") ||
+               nowWidget->inherits("QTimeEdit") ||
+               nowWidget->inherits("QDateTimeEdit")) {
+        currentWidget = nowWidget;
+        currentEditType = "QWidget";
+        ShowPanel();
+    }
+}
+
+void Keyboard::changeType(QString type)
+{
+    if (type == "max") {
+        changeLetter(true);
+        ui->btnType->setText("大写");
+        ui->labInfo->setText("中文输入法--大写");
+        ui->btnOther12->setText("/");
+        ui->btnOther14->setText(":");
+        ui->btnOther17->setText(",");
+        ui->btnOther18->setText("\\");
+        ui->btnOther21->setText("\"");
+    } else if (type == "min") {
+        changeLetter(false);
+        ui->btnType->setText("小写");
+        ui->labInfo->setText("中文输入法--小写");
+        ui->btnOther12->setText("/");
+        ui->btnOther14->setText(":");
+        ui->btnOther17->setText(",");
+        ui->btnOther18->setText("\\");
+        ui->btnOther21->setText("\"");
+    }
+}
+
+void Keyboard::changeLetter(bool isUpper)
+{
+    QList<QPushButton *> btn = this->findChildren<QPushButton *>();
+    foreach (QPushButton * b, btn) {
+        if (b->property("btnLetter").toBool()) {
+            if (isUpper) {
+                b->setText(b->text().toUpper());
+            } else {
+                b->setText(b->text().toLower());
+            }
+        }
+    }
+}
+
+void Keyboard::btn_clicked()
+{
+    //如果当前焦点控件类型为空,则返回不需要继续处理
+    if (currentEditType == "") {
+        return;
+    }
+    QPushButton *btn = (QPushButton *)sender();
+    QString objectName = btn->objectName();
+    if (objectName == "btnType") {
+        if (currentType == "min") {
+            currentType = "max";
+        } else if (currentType == "max") {
+            currentType = "min";
+        }
+        changeType(currentType);
+    } else if (objectName == "btnDelete") {
+           deleteValue();
+    } else if (objectName == "btnClose") {
+        this->setVisible(false);
+    } else if (objectName == "btnSpace") {
+        insertValue(" ");
+    }else if(objectName == "btnMax" || objectName == "btnMin"){
+        insertValue("");
+    }
+      else if(objectName == "Enter"){
+            insertValue("\n");
+    }
+    else {
+        QString value = btn->text();
+        //如果是&按钮，因为对应&被过滤,所以真实的text为去除前面一个&字符
+        if (objectName == "btnOther7") {
+            value = value.right(1);
+        }
+        //当前不是中文模式,则单击按钮对应text为传递参数
+        if (currentType != "chinese") {
+            insertValue(value);
+        }
+    }
+}
+
+void Keyboard::insertValue(QString value)
+{
+    if (currentEditType == "QLineEdit") {
+        currentLineEdit->insert(value);
+    } else if (currentEditType == "QTextEdit") {
+        currentTextEdit->insertPlainText(value);
+    } else if (currentEditType == "QPlainTextEdit") {
+        currentPlain->insertPlainText(value);
+    } else if (currentEditType == "QTextBrowser") {
+        currentBrowser->insertPlainText(value);
+    } else if (currentEditType == "QWidget") {
+        QKeyEvent keyPress(QEvent::KeyPress, 0, Qt::NoModifier, QString(value));
+        QApplication::sendEvent(currentWidget, &keyPress);
+    }
+}
+
+void Keyboard::deleteValue()
+{
+    if (currentEditType == "QLineEdit") {
+        currentLineEdit->backspace();
+    } else if (currentEditType == "QTextEdit") {
+        //获取当前QTextEdit光标,如果光标有选中,则移除选中字符,否则删除光标前一个字符
+        QTextCursor cursor = currentTextEdit->textCursor();
+        if(cursor.hasSelection()) {
+            cursor.removeSelectedText();
+        } else {
+            cursor.deletePreviousChar();
+        }
+    } else if (currentEditType == "QPlainTextEdit") {
+        //获取当前QTextEdit光标,如果光标有选中,则移除选中字符,否则删除光标前一个字符
+        QTextCursor cursor = currentPlain->textCursor();
+        if(cursor.hasSelection()) {
+            cursor.removeSelectedText();
+        } else {
+            cursor.deletePreviousChar();
+        }
+    } else if (currentEditType == "QTextBrowser") {
+        //获取当前QTextEdit光标,如果光标有选中,则移除选中字符,否则删除光标前一个字符
+        QTextCursor cursor = currentBrowser->textCursor();
+        if(cursor.hasSelection()) {
+            cursor.removeSelectedText();
+        } else {
+            cursor.deletePreviousChar();
+        }
+    } else if (currentEditType == "QWidget") {
+        QKeyEvent keyPress(QEvent::KeyPress, Qt::Key_Delete, Qt::NoModifier, QString());
+        QApplication::sendEvent(currentWidget, &keyPress);
+    }
+}
+
+void Keyboard::ChangeFont()
+{
+    QFont btnFont(this->font().family(), btnFontSize);
+    QFont labFont(this->font().family(), labFontSize);
+
+    QList<QPushButton *> btns = ui->widgetMain->findChildren<QPushButton *>();
+    foreach (QPushButton * btn, btns) {
+        btn->setFont(btnFont);
+    }
+
+    QList<QLabel *> labs = ui->widgetTop->findChildren<QLabel *>();
+    foreach (QLabel * lab, labs) {
+        lab->setFont(labFont);
+    }
+    ui->btnPre->setFont(labFont);
+    ui->btnNext->setFont(labFont);
+    ui->btnClose->setFont(labFont);
+}
+
+
+void Keyboard::on_btnMin_clicked()
+{
+    this->setGeometry(200,50,frmWidth,frmHeight);
+}
+
+void Keyboard::on_btnMax_clicked()
+{
+    this->setGeometry(200,50,frmWidth*2, frmHeight*2);
+}
---
+++ keyboard.h
@@ -0,0 +1,86 @@
+﻿#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#include <QWidget>
+#include <QMouseEvent>
+#include <QLabel>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QTextEdit>
+#include <QPlainTextEdit>
+#include <QTextBrowser>
+#include <QPushButton>
+#include <QTimer>
+#include "keyboard_global.h"
+namespace Ui
+{
+    class Keyboard;
+}
+
+class KEYBOARDSHARED_EXPORT Keyboard : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Keyboard(QWidget *parent = 0);
+    ~Keyboard();
+
+    void Init(int btnFontSize = 10, int labFontSize = 10);
+    void bindWidget(QWidget *nowWidget); //绑定窗口
+
+
+protected:
+    //鼠标拖动事件
+    void mouseMoveEvent(QMouseEvent *e);
+    //鼠标按下事件
+    void mousePressEvent(QMouseEvent *e);
+    //鼠标松开事件
+    void mouseReleaseEvent(QMouseEvent *);
+
+private slots:
+    //输入法面板按键处理
+    void btn_clicked();
+
+
+    void on_btnMin_clicked();
+
+    void on_btnMax_clicked();
+
+private:
+    Ui::Keyboard *ui;
+
+
+    int deskWidth;                  //桌面宽度
+    int deskHeight;                 //桌面高度
+    int frmWidth;                   //窗体宽度
+    int frmHeight;                  //窗体高度
+
+    QPoint mousePoint;              //鼠标拖动自定义标题栏时的坐标
+    bool mousePressed;              //鼠标是否按下
+
+    void InitForm();                //初始化窗体数据
+    void InitProperty();            //初始化属性
+    void ChangeFont();              //改变字体大小
+    void ShowPanel();               //显示输入法面板
+
+    QWidget *currentWidget;         //当前焦点的对象
+    QLineEdit *currentLineEdit;     //当前焦点的单行文本框
+    QTextEdit *currentTextEdit;     //当前焦点的多行文本框
+    QPlainTextEdit *currentPlain;   //当前焦点的富文本框
+    QTextBrowser *currentBrowser;   //当前焦点的文本浏览框
+
+    QString currentEditType;        //当前焦点控件的类型
+    QString currentPosition;        //当前输入法面板位置类型
+    int btnFontSize;                //当前输入法面板按钮字体大小
+    int labFontSize;                //当前输入法面板标签字体大小
+    void insertValue(QString value);//插入值到当前焦点控件
+    void deleteValue();             //删除当前焦点控件的一个字符
+
+    QString currentType;            //当前输入法类型
+    void changeType(QString type);  //改变输入法类型
+    void changeLetter(bool isUpper);//改变字母大小写
+
+
+};
+
+#endif // FRMINPUT_H
